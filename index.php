e96e4cba095ace6f65a42c03fe6fd83a071065f8
deprode <sensitively+github@gmail.com>
2015-10-06 14:47:05 +0900

    Add browser test

--- index.php
+++ index.php
@@ -1,9 +1,18 @@
 <?php
-    echo "Hello World!";
-
 function add($a, $b)
 {
     return $a + $b;
 }
-
-    echo add(1, 2);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>テスト</title>
+</head>
+<body>
+    <h1>テストです。</h1>
+    <p>1 + 2 = <?php echo add(1, 2); ?></p>
+</body>
+</html>
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -11,4 +11,5 @@ spl_autoload_register(
     }
 );
 
+require_once __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../index.php';
